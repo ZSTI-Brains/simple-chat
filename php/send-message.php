fba19504682cf95b5870ebb45de8de37fb0e0873
thethx2k <wojtasmarcin.20@gmail.com>
2019-09-19 14:04:43 +0200

    Changed index.html and added some files.

---
+++ php/send-message.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("db-connection.php");
+
+    if (isset($_POST["message"])) {
+        $message = $_POST["message"];
+        $date = date("Y-m-d H:i:s");
+
+        $mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
+
+        $query = "INSERT INTO chat_messages VALUES ('', 2, 1, '$message', '$date')";
+        $result = $mysqli->query($query);
+
+        $mysqli->close();
+    }
+?>
