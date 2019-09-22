828779d5a43876a875c7ba96b6b92be0fc2bf193
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-22 13:53:26 +0200

    Files change

---
+++ sign-in/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <title>Simple Chat</title>
+  </head>
+  <body>
+    <main>
+
+    </main>
+  </body>
+</html>
