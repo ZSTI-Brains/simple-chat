7b8f07cd0da67430e357b910aa10a7f57fc1c8fa
Jakub Szaredko <kuba.szaredko@gmail.com>
2019-09-23 17:35:34 +0200

    Visual changes

--- sign-in/index.php
+++ sign-in/index.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap">
-    <link rel="stylesheet" href="fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/jquery-3.4.1.min.js"></script>
     <title>Simple Chat</title>
   </head>
-  <body>
+  <body id="sign-in">
     <main>
+        <h1>SIMPLE CHAT</h1>
+        <form action="sign-in.php" method="post">
+            <div class="row">
+                <input type="text" placeholder="Username">
+            </div>
+            <div class="row">
+                <input type="password" placeholder="Password">
+            </div>
+            <div class="row">
+                <input type="submit" value="SIGN IN">
+            </div>
+            <div class="row">
+                <p>Don't you have an account? <a href="../sign-up">Create it now.</a></p>
+            </div>
+        </form>
 
     </main>
   </body>
